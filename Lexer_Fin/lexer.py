4a3c164f9acaf8782b0c284098b3184f0244298c
Gabriela <ferblanco1003@gmail.com>
2018-09-18 20:45:11 -0500

    Add files via upload

--- Lexer_Fin/lexer.py
+++ Lexer_Fin/lexer.py
@@ -2,17 +2,27 @@ import ply.lex as lex
 
 tokens = [ 'NAME','NUMBER','PLUS','MINUS','TIMES','DIVIDE', 'EQUALS' ]
 
+reserved = {'SUM' : 'plus', 'RES':'minus', 'MUL':'times', 'DIV':'divide'}
+
+tokens += reserved.values()
 t_ignore = ' \t'
-t_PLUS = r'\:SUM'
-t_MINUS = r':RES'
-t_TIMES = r'\:MUL'
-t_DIVIDE = r':DIV'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
 t_EQUALS = r':='
-t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
+    return t
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    if t.value in reserved:
+        t.type = reserved[t.value]
     return t
 
 # Error handling rule
@@ -22,7 +32,7 @@ def t_error(t):
 
 lex.lex() # Build the lexer
 
-lex.input("x = 3 :SUM 4 :RES 5 :MUL 6 :DIV 10")
+lex.input("x = 3 + 4 RES 5 MUL 6 DIV 10")
 while True:
     tok = lex.token()
     if not tok: break
